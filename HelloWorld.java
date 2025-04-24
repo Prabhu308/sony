0c8844cd988bf1b807430ca1c94c4cde726709d8
Prabhu <prabu502@gmail.com>
2025-04-24 17:02:47 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,21 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int a=5;
+        if(a>0)
+        {
+            System.out.println("positive");
+        }
+else if(a<0)
+{
+    System.out.println("negative");
+}
+        else
+{
+    System.out.println("zero");
+}
+        
         System.out.println("Hello, World!");
+        System.out.println("vikramadhithya");
+        
     }
 }
